bb02926213c865689e9eed78dc51fd272428d454
ɧσℓσ <paulinsegura79@gmail.com>
2022-11-24 22:01:09 +0100

    new account system

--- modules/accounts_manager.py
+++ modules/accounts_manager.py
@@ -1,42 +1,118 @@
 from flask import *  # for flash messages
 
-from bdd.bdd import Accounts  # Accounts database gestion
+from bdd.bdd import Database  # Accounts database gestion
 import bcrypt  # for password hashing
 
 
 class AccountsManager:
     """Classe pour gérer les comptes"""
-    def __init__(self, path):
+
+    def __init__(self):
         """Constructeur"""
-        self.accounts = Accounts(path)
+        self.accounts_table = Database().accounts
+        self.accounts = []
 
     def login(self, request):
         """Connecte un utilisateur"""
         username = request.form.get("username")
         password = request.form.get("password")
-        if account := self.accounts.get_account(username):
-            account = account[0]
-            if bcrypt.checkpw(password.encode('utf8'), account[2].encode('utf8')):
-                session["username"] = username
-                return redirect(url_for("accueil"))
+        if account_bdd := self.accounts_table.get_account(username):
+            account_bdd = account_bdd[0]
+            if bcrypt.checkpw(password.encode('utf8'), account_bdd[2].encode('utf8')):
+                self.add_user(username)
+                return redirect(url_for("index"))
             else:
                 flash("Mot de passe incorrect", "error")
         else:
             flash("Cet utilisateur n'existe pas", "error")
-        return redirect(url_for("accueil"))
+        return redirect(url_for("index"))
 
     def logout(self):
         """Déconnecte un utilisateur"""
+        self.remove_user(session["username"])
         session.pop("username", None)
-        return redirect(url_for("accueillir"))
+        return redirect(url_for("index"))
 
     def register(self, request):
         """Enregistre un utilisateur"""
         username = request.form.get("username")
         password = request.form.get("password")
-        if self.accounts.get_account(username):
+        if self.accounts_table.get_account(username):
             flash("Cet utilisateur existe déjà", "error")
         else:
-            self.accounts.add_account(username, bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()).decode('utf8'))
-            flash("Compte créé", "success")
-        return redirect(url_for("accueillir"))
+            self.accounts_table.add_account(username,
+                                            bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()).decode('utf8'))
+            self.add_user(username)
+        return redirect(url_for("index"))
+
+    def add_user(self, username):
+        session["username"] = username
+
+        account = Account(username)
+        account.refresh()
+
+        if self.get_account(username):
+            self.accounts.remove(self.get_account(username))
+
+        self.accounts.append(account)
+        flash("Vous êtes connecté", "success")
+
+    def remove_user(self, username):
+        self.accounts.remove(self.get_account(username))
+
+    def get_account(self, username):
+        return next((account for account in self.accounts if account.username == username), False)
+
+
+class Account:
+    """Classe pour un compte utilisateur"""
+
+    def __init__(self, username):
+        """Constructeur"""
+        self.username = username
+        self.user_id = None
+
+        self.tasks = []
+
+    def refresh(self):
+        """Rafraîchit les données de l'utilisateur"""
+        accounts_table = Database().accounts
+        tasks_table = Database().tasks
+
+        self.set_user_id(accounts_table.get_account(self.username)[0][0])
+
+        tasks = tasks_table.get_tasks(self.user_id)
+        for task in tasks:
+            self.add_task(task[0], task)
+
+    def add_task(self, id, task):
+        """Ajoute une tâche à l'utilisateur"""
+        self.tasks.append((id, task))
+
+    def remove_task(self, id):
+        """Supprime une tâche de l'utilisateur"""
+        for task in self.tasks:
+            if task[0] == id:
+                self.tasks.remove(task)
+                break
+
+    def update_task(self, id, **kwargs):
+        """Met à jour une tâche"""
+        tasks_table = Database().tasks
+        tasks_table.edit_user_task(self.user_id, id, **kwargs)
+
+    def get_tasks(self):
+        """Renvoie la liste des tâches de l'utilisateur"""
+        return self.tasks
+
+    def get_username(self):
+        """Renvoie le nom d'utilisateur"""
+        return self.username
+
+    def get_user_id(self):
+        """Renvoie l'identifiant de l'utilisateur"""
+        return self.user_id
+
+    def set_user_id(self, id):
+        """Définit l'identifiant de l'utilisateur"""
+        self.user_id = id
