8644e63307acb508ae7ee6f6fc991ca85a63acc5
Holo <paulinsegura79@gmail.com>
2022-11-24 15:51:26 +0100

    changing new main class

--- main.py
+++
@@ -1,5 +0,0 @@
-import todolist
-
-if __name__ == '__main__':
-    website = todolist.ToDoList()
-    website.run()
--- todolist.py
+++ todolist.py
@@ -7,28 +7,25 @@ gérant une liste de tâches à faire
 # Librairie(s) utilisée(s)
 from flask import *
 import secrets
-import bdd.bdd as bdd
+
+from bdd.bdd import Database
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+app.secret_key = secrets.token_urlsafe(16)
+
+# Initialisation de la base de données
+bdd_path = "bdd/todo.sqlite"
+bdd = Database(bdd_path)
 
 
-class ToDoList:
+@app.route("/")
+def accueil():
+    """Page d'accueil"""
+    return render_template("accueil.html")
 
-    def __init__(self):
-        """Constructeur"""
-        self.app = Flask(__name__, template_folder="templates", static_folder="static")
-        self.app.secret_key = secrets.token_urlsafe(16)
 
-        # Initialisation de la base de données
-        self.bdd_path = "bdd/todo.sqlite"
-
-        self.accounts = bdd.Accounts(self.bdd_path)
-        self.tasks = bdd.Tasks(self.bdd_path)
-        self.types = bdd.Types(self.bdd_path)
-        self.priorities = bdd.Priorities(self.bdd_path)
-        self.states = bdd.States(self.bdd_path)
-
-    def run(self):
-        """Lance le serveur web"""
-        self.app.run(host="0.0.0.0", port=1664, threaded=True, debug=True)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=1664, threaded=True, debug=True)
 
 
 
