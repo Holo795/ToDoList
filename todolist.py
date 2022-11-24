b1c64c34e725f8049f228466c4f58bf80f0621f4
ɧσℓσ <paulinsegura79@gmail.com>
2022-11-24 22:01:19 +0100

    import accounts_manager

--- todolist.py
+++ todolist.py
@@ -5,6 +5,7 @@ gérant une liste de tâches à faire
 """
 
 import secrets
+from datetime import timedelta
 
 # Librairie(s) utilisée(s)
 from flask import *
@@ -15,24 +16,44 @@ from modules import accounts_manager
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.secret_key = secrets.token_urlsafe(16)
 
-# Initialisation de la base de données
-bdd_path = "bdd/todo.sqlite"
-bdd = Database(bdd_path)
+# Initialisation du gestionnaire de comptes
+accounts_manager = accounts_manager.AccountsManager()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return redirect(url_for("index"))
+
+
+@app.before_request
+def check_login():
+    """Vérifie si l'utilisateur est connecté"""
+    if request.endpoint not in ["index", "logout", "login", "register", "static", None] and \
+            accounts_manager.get_account(session["username"]) is None:
+        return redirect(url_for("index"))
 
 
 @app.route("/")
-def accueil():
+def index():
     """Page d'accueil"""
-    return render_template("accueil.html", login=session.get("USERNAME") is not None)
+    user = accounts_manager.get_account(session.get("username"))
+
+    return render_template("accueil.html", user=user)
 
 
 @app.route("/login", methods=["post"])
 def login():
-    user = accounts_manager.AccountsManager(path=bdd_path)
-    return user.login(request)
+    return accounts_manager.login(request)
 
 
+@app.route("/logout")
+def logout():
+    return accounts_manager.logout()
 
+
+@app.route("/register", methods=["post"])
+def register():
+    return accounts_manager.register(request)
 
 
 def show_task():
