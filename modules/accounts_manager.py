7d97bfb0f5a88099c5a0531aed87a2a630a4982d
ɧσℓσ <paulinsegura79@gmail.com>
2022-11-26 14:54:06 +0100

    Adding types of arguments and returns

--- modules/accounts_manager.py
+++ modules/accounts_manager.py
@@ -12,7 +12,7 @@ class AccountsManager:
         self.accounts_table = Database().accounts
         self.accounts = []
 
-    def login(self, request):
+    def login(self, request: Request) -> Response:
         """Connecte un utilisateur"""
         username = request.form.get("username")
         password = request.form.get("password")
@@ -27,13 +27,13 @@ class AccountsManager:
             flash("Mot de passe et/ou identifiant incorrect", "error")
         return redirect(url_for("index"))
 
-    def logout(self):
+    def logout(self) -> Response:
         """Déconnecte un utilisateur"""
         self.remove_user(session["username"])
         session.pop("username", None)
         return redirect(url_for("index"))
 
-    def register(self, request):
+    def register(self, request: Request) -> Response:
         """Enregistre un utilisateur"""
         username = request.form.get("username")
         password = request.form.get("password")
@@ -45,7 +45,7 @@ class AccountsManager:
             self.add_user(username)
         return redirect(url_for("index"))
 
-    def add_user(self, username):
+    def add_user(self, username: str):
         session["username"] = username
 
         account = Account(username)
@@ -57,17 +57,17 @@ class AccountsManager:
         self.accounts.append(account)
         flash("Vous êtes connecté", "success")
 
-    def remove_user(self, username):
+    def remove_user(self, username: str):
         self.accounts.remove(self.get_account(username))
 
-    def get_account(self, username):
+    def get_account(self, username: str):
         return next((account for account in self.accounts if account.username == username), False)
 
 
 class Account:
     """Classe pour un compte utilisateur"""
 
-    def __init__(self, username):
+    def __init__(self, username: str):
         """Constructeur"""
         self.username = username
         self.user_id = None
@@ -85,34 +85,34 @@ class Account:
         for task in tasks:
             self.add_task(task)
 
-    def add_task(self, task):
+    def add_task(self, task: tuple):
         """Ajoute une tâche à l'utilisateur"""
         self.tasks.append(task)
 
-    def remove_task(self, id):
+    def remove_task(self, id: int):
         """Supprime une tâche de l'utilisateur"""
         for task in self.tasks:
             if task[0] == id:
                 self.tasks.remove(task)
                 break
 
-    def update_task(self, id, **kwargs):
+    def update_task(self, id: int, **kwargs: dict):
         """Met à jour une tâche"""
         tasks_table = Database().tasks
         tasks_table.edit_user_task(self.user_id, id, **kwargs)
 
-    def get_tasks(self):
+    def get_tasks(self) -> list:
         """Renvoie la liste des tâches de l'utilisateur"""
         return self.tasks
 
-    def get_username(self):
+    def get_username(self) -> str:
         """Renvoie le nom d'utilisateur"""
         return self.username
 
-    def get_user_id(self):
+    def get_user_id(self) -> int:
         """Renvoie l'identifiant de l'utilisateur"""
         return self.user_id
 
-    def set_user_id(self, id):
+    def set_user_id(self, id: int):
         """Définit l'identifiant de l'utilisateur"""
         self.user_id = id
