81a10b257ca5ed2016ab122e1e859a1ba5aaf206
ɧσℓσ <paulinsegura79@gmail.com>
2022-11-27 16:56:06 +0100

    Change Time system management

--- modules/tasks_utils.py
+++ modules/tasks_utils.py
@@ -1,4 +1,5 @@
-from modules.date_format import DateMicros
+import locale
+from datetime import datetime
 
 
 class TasksUtils:
@@ -13,10 +14,60 @@ class TasksUtils:
             "id": task[0],
             "title": task[2],
             "description": task[3],
-            "date": DateMicros(task[4]).get_date_text(),
-            "done": DateMicros(task[5]).get_date_text() if task[5] else None,
-            "time_left": DateMicros(task[4]).get_difference_text(),
+            "date": TasksTimeUtils(task[4]).get_date_text(),
+            "done": TasksTimeUtils(task[5]).get_date_text() if task[5] else None,
+            "time_left": TasksTimeUtils(task[4]).get_difference_text(),
             "type": task[6],
             "priority": task[7],
             "category": task[8]
         }
+
+
+class TasksTimeUtils:
+    """Cette classe permet de convertir tout type de date en format datetime"""
+    def __init__(self, date):
+        """Constructeur"""
+
+        locale.setlocale(locale.LC_TIME, "fr_FR")  # Permet d'avoir les jours et les mois en français
+
+        if type(date) == str:
+            self.date = datetime.strptime(date, "%Y-%m-%dT%H:%M")
+        elif type(date) in [int, float]:
+            self.date = datetime.fromtimestamp(int(date))
+        elif type(date) == datetime:
+            self.date = date
+        else:
+            raise TypeError("Type de date non reconnu")
+
+    def get_date(self):
+        return self.date
+
+    def get_date_text(self):
+        return self.date.strftime("%A %d %B %Y %Hh%M").capitalize()
+
+    def get_difference(self, date=None):
+        if date is None:
+            date = datetime.now()
+        return self.date - date
+
+    def get_difference_text(self, date=None):
+        if date is None:
+            date = datetime.now()
+        difference = self.get_difference(date)
+        text = "" + ("Il y a " if self.date.timestamp() <= date.timestamp() else "Dans ")
+        difference = abs(difference)
+        if difference.days > 0:
+            text += f"{difference.days} jour(s) "
+        if difference.seconds > 0:
+            text += f"{difference.seconds // 3600} heure(s) "
+        if difference.seconds % 3600 > 0:
+            text += f"{(difference.seconds % 3600) // 60} minute(s) "
+        if difference.seconds % 60 > 0:
+            text += f"{difference.seconds % 60} seconde(s) "
+        return text
+
+    def get_microseconds(self):
+        return int(self.date.timestamp())
+
+
+
