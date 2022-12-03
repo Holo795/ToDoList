30e329690637a4abf0fa5c2e2ff32ea2a0187801
ɧσℓσ <paulinsegura79@gmail.com>
2022-12-03 01:21:04 +0100

    show notification after allow

--- static/scripts/notification.js
+++ static/scripts/notification.js
@@ -1,15 +1,21 @@
 function notifier(title="ToDo List", message, icon_image=null, bg_image=null) {
     /* On demande la permission si les notifications ne sont pas permises */
     if (Notification.permission !== 'granted')
-        Notification.requestPermission();
+        Notification.requestPermission().then(r => {
+            if (r === 'granted') {
+                new Notification(title, {
+                    body: message,
+                    icon: icon_image,
+                    image: bg_image
+                });
+            }
+        });
     /* Sinon on affiche la notification */
     else {
-
-        var notification = new Notification(title, {
+        new Notification(title, {
             icon: icon_image,
             body: message,
             image: bg_image,
         });
-
     }
 }
